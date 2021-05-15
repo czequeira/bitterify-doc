3129eb0c090058471cf5fdc08f6e688b1f15e839
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-05-15 18:02:50 -0400

    Creado el hero section

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,3 +1,32 @@
-import { h3 } from 'bitterify/lib';
+import { a, b, div, img, p, section } from 'bitterify/lib';
 
-export const Home = h3('Home');
+const HeroSection = section([
+  div().setClasses(
+    'w-full h-full top-0 left-0 absolute bg-gray-500 opacity-50',
+  ),
+  div([
+    b('A frontend framework without tags').setClasses(
+      'text-2xl sm:text-4xl md:text-6xl',
+    ),
+    p(['mas texto aqui y un poco mas largo con mejor explicacion']).setClasses(
+      'text-lg sm:text-xl md:text-2xl',
+    ),
+    div([
+      a('Getting started', '#').setClasses(
+        'w-full text-center px-4 py-2 m-2 rounded bg-red-500 hover:bg-red-600 shadow',
+      ),
+      a('Github', '#').setClasses(
+        'w-full text-center px-4 py-2 m-2 rounded bg-gray-500 hover:bg-gray-600 shadow',
+      ),
+    ]).setClasses(
+      'flex flex-col w-full p-4 sm:block text-center sm:text-lg md:text-2xl',
+    ),
+  ]).setClasses(
+    'h-full w-full px-8 md:px-24 text-center flex flex-col absolute top-0 left-0 justify-center items-center',
+  ),
+  img('images/pexels-tom-cordner-1399097.jpg').setClasses(
+    'md:container mx-auto',
+  ),
+]).setClasses('w-full relative text-white');
+
+export const Home = HeroSection;
