4bc7290b719a1840a30807839caff160527689be
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-05-15 18:58:37 -0400

    Modificado el hero section

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -8,24 +8,24 @@ const HeroSection = section([
     b('A frontend framework without tags').setClasses(
       'text-2xl sm:text-4xl md:text-6xl',
     ),
-    p(['mas texto aqui y un poco mas largo con mejor explicacion']).setClasses(
+    p(['Bitterify propouse a way to develop web apps']).setClasses(
       'text-lg sm:text-xl md:text-2xl',
     ),
     div([
       a('Getting started', '#').setClasses(
-        'w-full text-center px-4 py-2 m-2 rounded bg-red-500 hover:bg-red-600 shadow',
+        'w-full text-center px-4 py-2 m-1 rounded bg-red-500 hover:bg-red-600 shadow',
       ),
       a('Github', '#').setClasses(
-        'w-full text-center px-4 py-2 m-2 rounded bg-gray-500 hover:bg-gray-600 shadow',
+        'w-full text-center px-4 py-2 m-1 rounded bg-gray-500 hover:bg-gray-600 shadow',
       ),
     ]).setClasses(
-      'flex flex-col w-full p-4 sm:block text-center sm:text-lg md:text-2xl',
+      'flex flex-col w-full p-2 sm:block text-center sm:text-lg md:text-2xl',
     ),
   ]).setClasses(
     'h-full w-full px-8 md:px-24 text-center flex flex-col absolute top-0 left-0 justify-center items-center',
   ),
-  img('images/pexels-tom-cordner-1399097.jpg').setClasses(
-    'md:container mx-auto',
+  img('images/pexels-tom-cordner-1399097-min.jpg').setClasses(
+    'md:container mx-auto ',
   ),
 ]).setClasses('w-full relative text-white');
 
